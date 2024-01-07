cece0cab84df16c8acb3bf8876a8ca06d32d6e09
bruce <cnhxunoo@gmail.com>
2024-01-07 20:36:14 +0800

    add helper specification

--- retrosyn/src/renderer/src/pages/Help.tsx
+++ retrosyn/src/renderer/src/pages/Help.tsx
@@ -4,10 +4,11 @@ const Help: React.FC = () => {
   return (
     <Flex
       direction="column"
-      width="100%"
+      width="50%"
       height="100%"
       justify="start"
-      align="center"
+      align="start"
+      mx={40}
       gap={3}
       py={5}
     >
@@ -17,6 +18,11 @@ const Help: React.FC = () => {
           在线使用 aichem.pylogic.net
         </Link>
       </Text>
+      <Text>1. 正常输入Smiles来查询合成路线, 可以画出结构图来查询</Text>
+      <Text>2. 可以查询反应路线和反应条件</Text>
+      <Text>3. 反应路线图包含化合物节点（显示结构图)， 和反应节点</Text>
+      <Text>4. 编辑反应路线可以选择 反应节点， 按Del键来删除部分反应路线</Text>
+      <Text>5. 多使用几次，就熟悉了:)</Text>
     </Flex>
   )
 }
